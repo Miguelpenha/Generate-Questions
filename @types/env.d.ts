818aa91889d94aee03df35f6426d74730956408c
Miguel Penha <miguel.penha1@gmail.com>
2024-12-27 02:16:38 -0300

    Fix vars

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -1,9 +1,9 @@
 declare global {
     namespace NodeJS {
       interface ProcessEnv {
+        API_QUESTIONS_URL: string
+        API_QUESTIONS_KEY: string
         NEXT_PUBLIC_API_KEY: string
-        NEXT_PUBLIC_API_QUESTIONS_URL: string
-        NEXT_PUBLIC_API_QUESTIONS_KEY: string
       }
     }
 }
--- services/apiQuestions.ts
+++ services/apiQuestions.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const apiQuestions = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_QUESTIONS_URL,
+    baseURL: process.env.API_QUESTIONS_URL,
     headers: {
-        authorization: `key ${process.env.NEXT_PUBLIC_API_QUESTIONS_KEY}`
+        authorization: `key ${process.env.API_QUESTIONS_KEY}`
     }
 })
 
